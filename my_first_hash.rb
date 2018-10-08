e17b8a57de4c867aa3ed09ceb332808cfc275004
Robert Schulte <schulte.robertw@outlook.com>
2018-10-08 10:07:16 -0500

    added do keyword

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,14 @@
-def my_hash
+def my_hash do
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   example = {
     "my name" => "Robert Schulte"
     "my age" => 28
   }
-
+  return my_hash
 end
 
 
-def shipping_manifest
+def shipping_manifest do
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
@@ -21,7 +21,7 @@ def shipping_manifest
 
 end
 
-def retrieval
+def retrieval do
   shipping_manifest = {
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
@@ -33,7 +33,7 @@ def retrieval
 
 end
 
-def adding
+def adding do 
   shipping_manifest = {
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
